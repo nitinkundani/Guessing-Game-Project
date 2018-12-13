0d37ae12163357e6df6d9a884cef34443aa626ba
nitinkundani <36822967+nitinkundani@users.noreply.github.com>
2018-12-13 21:37:29 +0530

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -4,24 +4,21 @@
 no_files=$(ls | wc -l)
 guess=-1
 
-while [[ guess -ne no_files ]]
+while [[ $guess -ne $no_files ]]
 do
 
 echo "How many files are in the current directory ?"
 read arg
 guess=$arg
 
-if [[ $guess -gt no_files ]] 
+if [[ $guess -gt $no_files ]] 
 then
 echo "Too high"
-elif [[ $guess -lt no_files ]]
+elif [[ $guess -lt $no_files ]]
 then
 echo "Too low"
-elif [[ $guess -eq no_files ]]
-then
-echo "Congratulations, your guess is correct."
 else
-echo "Invalid input"
+echo "Congratulations, your guess is correct."
 fi
 
 done
